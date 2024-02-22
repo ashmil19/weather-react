8e4cfb62f8e577a225f6c2cc9faa9d1b01b55c17
Ashmil Abdul Rasheed <ashmilabdulrasheed@gmail.com>
2024-02-22 15:43:02 +0530

    https

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,1 @@
-export const BASEURL = 'http://api.openweathermap.org/data/2.5/weather?appid=b431678917774e539c55ce9d4a324de6'
+export const BASEURL = 'https://api.openweathermap.org/data/2.5/weather?appid=b431678917774e539c55ce9d4a324de6'
